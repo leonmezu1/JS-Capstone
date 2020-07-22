df7c4c2ac960be9213b811f1b0f8e399f6ab827e
leonmezu1 <leo.mez@outlook.com>
2020-07-21 20:10:33 -0500

    Trimmed template

--- src/js/Scenes/bootScene.js
+++ src/js/Scenes/bootScene.js
@@ -8,23 +8,5 @@ export default class BootScene extends Scene {
     });
   }
 
-  preload() {
-    this.load.image('ball', 'assets/ball.png');
-    this.load.image('left', 'assets/left_pallete.png');
-    this.load.image('right', 'assets/right_pallete.png');
-    this.load.image('separator', 'assets/separator.png');
-    const loadingBar = this.add.graphics({
-      fillStyle: {
-        color: 0xffffff,
-      },
-    });
-    this.load.on('progress', percent => {
-      loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
-    });
-    this.load.on('complete', () => {
-      setTimeout(() => {
-        this.scene.start(Handler.scenes.main);
-      }, 1000);
-    });
-  }
+  preload() {}
 }
--- src/js/Scenes/mainScene.js
+++ src/js/Scenes/mainScene.js
@@ -10,50 +10,7 @@ export default class MainScene extends Scene {
     });
   }
 
-  chochaPala() {
-    this.ball.body.setVelocityY(Math.Between(-120, 120));
-  }
+  create() {}
 
-  create() {
-    const gameHeight = this.game.renderer.height;
-    const gameWidth = this.game.renderer.width;
-    this.add.image(gameWidth / 2, gameHeight / 2, 'separator').setScale(1.45);
-    this.izquierda = new Palas(this, 50, gameHeight / 2, 'left');
-    this.derecha = new Palas(this, gameWidth - 50, gameHeight / 2, 'right');
-    this.ball = new Bola(this, 400, 300, 'ball');
-    this.physics.add.collider(this.ball, this.izquierda, () => {
-      this.chochaPala();
-    });
-    this.physics.add.collider(this.ball, this.derecha, () => {
-      this.chochaPala();
-    });
-    this.cursor = this.input.keyboard.createCursorKeys();
-    this.keyboard = this.input.keyboard.addKeys('W, A, S, D');
-  }
-
-  update() {
-    if (this.ball.body.x < 0 || this.ball.body.x > 800) {
-      this.ball.setPosition(400, 300);
-    }
-
-    if (this.cursor.down.isDown) {
-      this.derecha.body.setVelocityY(300);
-    } else if (this.cursor.up.isDown) {
-      this.derecha.body.setVelocityY(-300);
-    }
-
-    if (this.keyboard.S.isDown) {
-      this.izquierda.body.setVelocityY(300);
-    } else if (this.keyboard.W.isDown) {
-      this.izquierda.body.setVelocityY(-300);
-    }
-
-    if (this.keyboard.W.isUp && this.keyboard.S.isUp) {
-      this.izquierda.body.setVelocityY(0);
-    }
-
-    if (this.cursor.up.isUp && this.cursor.down.isUp) {
-      this.derecha.body.setVelocityY(0);
-    }
-  }
+  update() {}
 }
