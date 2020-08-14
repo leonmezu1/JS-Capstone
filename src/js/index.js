c882da39f0736858954d1d4ba66ea7547f001776
leonmezu1 <leo.mez@outlook.com>
2020-08-13 21:27:05 -0500

    Changed menu Entry

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,4 @@
 import '../styles/styles.scss';
 import config from './config';
-import { fadeInEffect, fadeOutEffect } from './utils/domUtils';
-
-
-const { body } = document;
-const spinner = document.querySelector('#spinner');
-
-/* document.addEventListener('DOMContentLoaded', () => {
-  fadeInEffect(body, 800);
-  setTimeout(() => {
-    fadeInEffect(spinner, 200);
-  }, 100);
-  setTimeout(() => {
-    fadeOutEffect(spinner, 200);
-  }, 2000);
-}); */
-
 
 config();
